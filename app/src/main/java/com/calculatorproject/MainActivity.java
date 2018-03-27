b1a3d1ce51d7b2bffc202a58b5179e117c776ed5
david-denny <dpwdenny@gmail.com>
2018-03-27 15:47:23 +0100

    Add toolbar with settings button

--- app/src/main/java/com/calculatorproject/MainActivity.java
+++ app/src/main/java/com/calculatorproject/MainActivity.java
@@ -2,6 +2,12 @@ package com.calculatorproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ActionMode;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialise toolbar
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate toolbar_main xml file so buttons show up
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // TODO: send use to settings layout
+
+                return true;
+
+            default:
+                // User's action is unrecognised, invoke superclass to handle it
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
