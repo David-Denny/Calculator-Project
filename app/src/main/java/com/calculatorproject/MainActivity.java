ceece4e9c4925833a1eaae46e7705293296bfe4b
david-denny <dpwdenny@gmail.com>
2018-06-28 23:50:50 +0100

    Add main numerical buttons to main screen of calculator

--- app/src/main/java/com/calculatorproject/MainActivity.java
+++ app/src/main/java/com/calculatorproject/MainActivity.java
@@ -2,19 +2,17 @@ package com.calculatorproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.view.ActionMode;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.calc_main);
 
         // Initialise toolbar
         Toolbar toolbar = findViewById(R.id.my_toolbar);
