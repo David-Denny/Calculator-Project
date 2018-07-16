4e4bc7188d2d8beaa452a3e414d67ed463ee3017
david-denny <dpwdenny@gmail.com>
2018-07-16 19:31:53 +0100

    Add functionality to convert infix to Reverse Polish Notation postfix using the Shunting Yard algorithm

--- app/src/main/java/com/calculatorproject/MainActivity.java
+++ app/src/main/java/com/calculatorproject/MainActivity.java
@@ -3,16 +3,25 @@ package com.calculatorproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Stack;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String equation;
+    private TextView mCalculatorDisplay;
+    private ShuntingYard mShuntingYard;
+    private String mAnswer;
+    private String mInfix;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,9 +57,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.leftBracket).setOnClickListener(this);
         findViewById(R.id.rightBracket).setOnClickListener(this);
 
-        // initialise calculatorDisplay EditText
+        mCalculatorDisplay = findViewById(R.id.calculatorDisplay);
+
+        equation = "";
+
+        mShuntingYard = new ShuntingYard();
 
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -89,72 +103,78 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.enter0:
-                equation = equation + 0;
+                equation = equation + "0 ";
                 break;
 
             case R.id.enter1:
-                equation = equation + 1;
+                equation = equation + "1 ";
                 break;
 
             case R.id.enter2:
-                equation = equation + 2;
+                equation = equation + "2 ";
                 break;
 
             case R.id.enter3:
-                equation = equation + 3;
+                equation = equation + "3 ";
                 break;
 
             case R.id.enter4:
-                equation = equation + 4;
+                equation = equation + "4 ";
                 break;
 
             case R.id.enter5:
-                equation = equation + 5;
+                equation = equation + "5 ";
                 break;
 
             case R.id.enter6:
-                equation = equation + 6;
+                equation = equation + "6 ";
                 break;
 
             case R.id.enter7:
-                equation = equation + 7;
+                equation = equation + "7 ";
                 break;
 
             case R.id.enter8:
-                equation = equation + 8;
+                equation = equation + "8 ";
                 break;
 
             case R.id.enter9:
-                equation = equation + 9;
+                equation = equation + "9 ";
                 break;
 
             case R.id.leftBracket:
-                equation = equation + "(";
+                equation = equation + "( ";
                 break;
 
             case R.id.rightBracket:
-                equation = equation + ")";
+                equation = equation + ") ";
                 break;
 
             case R.id.enterADD:
-                equation = equation + "+";
+                equation = equation + "+ ";
                 break;
 
             case R.id.enterMULT:
-                equation = equation + "×";
+                equation = equation + "× ";
                 break;
 
             case R.id.enterDIVIDE:
-                equation = equation + "÷";
+                equation = equation + "÷ ";
                 break;
 
             case R.id.enterMINUS:
-                equation = equation + "−";
+                equation = equation + "- ";
                 break;
 
             case R.id.enterEQUALS:
                 // TODO: use ShuntingYard class to convert infix to postfix
+
+                mInfix = ShuntingYard.infixToPostfix(equation);
+
         }
+
+        mCalculatorDisplay.setText(equation);
     }
+
 
 }
--- app/src/main/java/com/calculatorproject/ShuntingYard.java
+++ app/src/main/java/com/calculatorproject/ShuntingYard.java
@@ -1,9 +1,66 @@
 package com.calculatorproject;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.Stack;
+
 public class ShuntingYard {
 
 
-    protected void infixToPostfix (String infix) {
 
+    static String infixToPostfix (String infix) {
+
+        final String ops = "-+÷×^";
+
+        StringBuilder stringBuilder = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : infix.split("\\s")) {
+            if (token.isEmpty()) {
+                continue;
+            }
+
+            char character = token.charAt(0);
+            int index = ops.indexOf(character);
+
+            if (index != -1) {
+                if (stack.isEmpty()) {
+                    stack.push(index);
+                } else {
+                    while (!stack.isEmpty()) {
+                        int prec2 = stack.peek() / 2;
+                        int prec1 = index / 2;
+
+                        if (prec2 > prec1 || (prec2 == prec1 && character != '^')) {
+                            stringBuilder.append(ops.charAt(stack.pop())).append(' ');
+                        } else {
+                            break;
+                        }
+                    }
+
+                    stack.push(index);
+                }
+            } else if (character == '(') {
+                stack.push(-2);
+            } else if (character == ')') {
+
+                while (stack.peek() != -2) {
+                    stringBuilder.append(ops.charAt(stack.pop())).append(' ');
+                }
+                stack.pop();
+            } else {
+                stringBuilder.append(token).append(' ');
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            stringBuilder.append(ops.charAt(stack.pop())).append(' ');
+        }
+
+        return stringBuilder.toString();
     }
 }
