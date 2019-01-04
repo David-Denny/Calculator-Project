c04436da42da9d939719b5bc00ef7aa855e31c22
david-denny <dpwdenny@gmail.com>
2019-01-04 18:15:07 +0000

    Add denary/binary and binary/hexadecimal conversions

--- app/src/main/java/com/calculatorproject/ConversionsRAdapter.java
+++ app/src/main/java/com/calculatorproject/ConversionsRAdapter.java
@@ -34,30 +34,32 @@ public class ConversionsRAdapter extends RecyclerView.Adapter<ConversionsRAdapte
                 case 0:
 
                     // create dialog variable
-                    final Dialog dialog = new Dialog(v.getContext());
+                    final Dialog dialogDenToBin = new Dialog(v.getContext());
 
                     // inflate dialog layout
-                    dialog.setContentView(R.layout.conversion_dialog);
+                    dialogDenToBin.setContentView(R.layout.conversion_dialog);
 
                     // initialise  buttons
-                    Button dialogBack = dialog.findViewById(R.id.dialog_cancel);
-                    Button dialogCalculate = dialog.findViewById(R.id.dialog_calculate);
+                    Button dialogBack = dialogDenToBin.findViewById(R.id.dialog_cancel);
+                    Button dialogCalculate = dialogDenToBin.findViewById(R.id.dialog_calculate);
 
                     // dismiss dialog when user clicks cancel button
                     dialogBack.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            dialog.dismiss();
+                            dialogDenToBin.dismiss();
                         }
                     });
-                    
+
+
                     dialogCalculate.setOnClickListener(new View.OnClickListener() {
 
                         @Override
                         public void onClick(View v) {
 
-                            EditText denaryEditText = dialog.findViewById(R.id.conversion_input_1);
-                            EditText binaryEditText = dialog.findViewById(R.id.conversion_input_2);
+                            // initialise variables
+                            EditText denaryEditText = dialogDenToBin.findViewById(R.id.conversion_input_1);
+                            EditText binaryEditText = dialogDenToBin.findViewById(R.id.conversion_input_2);
 
                             String denary = denaryEditText.getText().toString();
                             String binary = binaryEditText.getText().toString();
@@ -70,17 +72,147 @@ public class ConversionsRAdapter extends RecyclerView.Adapter<ConversionsRAdapte
                             } else if (binary.equals("")) {
 
                                 // convert denary to binary and display to user
-                                binary = String.valueOf(Integer.toBinaryString(Integer.valueOf(denary)));
+                                binary = String.valueOf(Integer.toBinaryString(Integer.
+                                        valueOf(denary)));
                                 binaryEditText.setText(binary);
                             } else {
 
-
+                                // alert user to their error
+                                Toast.makeText(v.getContext(),
+                                        "Leave one entry variable blank.",
+                                        Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
-                    
-                    dialog.show();
 
+                    // display dialog
+                    dialogDenToBin.show();
+                    break;
+
+                case 1:
+
+                    // create dialog variable
+                    final Dialog dialogDenToHex = new Dialog(v.getContext());
+
+                    // inflate dialog layout
+                    dialogDenToHex.setContentView(R.layout.conversion_dialog);
+
+                    // initialise  buttons
+                    Button dialogDenToHexBack = dialogDenToHex.findViewById(R.id.dialog_cancel);
+                    Button dialogDenToHexCalculate = dialogDenToHex.findViewById(R.id.dialog_calculate);
+
+                    // set different title
+                    TextView title2 = dialogDenToHex.findViewById(R.id.input_title_2);
+                    title2.setText(v.getResources().getString(R.string.hexadecimal));
+
+                    // dismiss dialog when user clicks cancel button
+                    dialogDenToHexBack.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialogDenToHex.dismiss();
+                        }
+                    });
+
+                    dialogDenToHexCalculate.setOnClickListener(new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View v) {
+
+                            EditText denaryEditText = dialogDenToHex.findViewById(R.id.conversion_input_1);
+                            EditText hexEditText = dialogDenToHex.findViewById(R.id.conversion_input_2);
+
+                            String denary = denaryEditText.getText().toString();
+                            String hex = hexEditText.getText().toString();
+
+                            if (denary.equals("")) {
+
+                                // convert and display hex to denary
+                                denary = String.valueOf(Integer.parseInt(hex, 16));
+                                denaryEditText.setText(denary);
+
+                            } else if (hex.equals("")) {
+
+                                // convert and display denary to hex
+                                hex = String.valueOf(Integer.toHexString(Integer.valueOf(denary)));
+                                hexEditText.setText(hex);
+
+                            } else {
+
+                                // display error message to user
+                                Toast.makeText(v.getContext(),
+                                        "Leave one entry variable blank.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                    // display dialog;
+                    dialogDenToHex.show();
+                    break;
+
+                case 2:
+
+                    // create dialog variable
+                    final Dialog dialogBinToHex = new Dialog(v.getContext());
+
+                    // inflate dialog layout
+                    dialogBinToHex.setContentView(R.layout.conversion_dialog);
+
+                    // initialise  buttons
+                    Button dialogBinToHexBack = dialogBinToHex.findViewById(R.id.dialog_cancel);
+                    Button dialogBinToHexCalculate = dialogBinToHex.findViewById(R.id.dialog_calculate);
+
+                    // set different titles
+                    TextView titleBin = dialogBinToHex.findViewById(R.id.input_title_1);
+                    TextView titleHex = dialogBinToHex.findViewById(R.id.input_title_2);
+
+                    titleBin.setText(v.getResources().getString(R.string.binary));
+                    titleHex.setText(v.getResources().getString(R.string.hexadecimal));
+
+                    // dismiss dialog when user clicks cancel button
+                    dialogBinToHexBack.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialogBinToHex.dismiss();
+                        }
+                    });
+
+                    dialogBinToHexCalculate.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                            EditText binaryEditText = dialogBinToHex.findViewById(R.id.conversion_input_1);
+                            EditText hexEditText = dialogBinToHex.findViewById(R.id.conversion_input_2);
+
+                            String binary = binaryEditText.getText().toString();
+                            String hex = hexEditText.getText().toString();
+
+                            if (binary.equals("")) {
+
+                                // convert hex to binary and display
+                                binary = Integer.toBinaryString(Integer.parseInt(hex, 16));
+                                binaryEditText.setText(binary);
+
+                            } else if (hex.equals("")) {
+
+                                // convert and display binary to hex
+                                hex = Integer.toString(Integer.parseInt(binary, 2), 16);
+                                hexEditText.setText(hex);
+
+                            } else {
+
+                                // display error message to user
+                                Toast.makeText(v.getContext(),
+                                        "Leave one entry variable blank.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+
+                    // display dialog to user
+                    dialogBinToHex.show();
+                    break;
             }
         }
     };
